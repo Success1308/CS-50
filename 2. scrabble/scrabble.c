2d7007ca122d64cb1e8bf83115917652ebca10ce
Success Ambaliya <success.ambaliya13@gmail.com>
2024-08-18 16:31:02 -0400

    Update scrabble.c

--- 2. scrabble/scrabble.c
+++ 2. scrabble/scrabble.c
@@ -7,6 +7,7 @@ int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 
 
 int compute_score(string word);
 
+
 int main(void)
 {
     string word1 = get_string("Player 1: ");
